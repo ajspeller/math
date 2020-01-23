6c3efe090b9b8de19ba147d96e4bee461dee1aa8
AJ Speller <ajspeller@gmail.com>
2020-01-23 00:09:44 -0500

    Refactor custom validator

--- src/app/math-validators.ts
+++ src/app/math-validators.ts
@@ -1,16 +1,17 @@
 import { AbstractControl } from '@angular/forms';
 
 export class MathValidators {
-  static addition(form: AbstractControl) {
-    const { a, b, answer } = form.value;
-    console.log(a + b);
-    console.log(answer);
-    console.log(a + b === +answer);
-    if (a + b === +answer) {
-      return null;
-    }
-    return {
-      addition: true
+  static addition(target: string, sourceOne: string, sourceTwo: string) {
+    return (form: AbstractControl) => {
+      const sum = form.value[target];
+      const op1 = form.value[sourceOne];
+      const op2 = form.value[sourceTwo] ;
+      if (op1 + op2 === +sum) {
+        return null;
+      }
+      return {
+        addition: true
+      };
     };
   }
 
